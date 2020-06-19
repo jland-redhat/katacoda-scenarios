56e271c058c7599186c15a66a6711ff53e775963
James Land <hokie10@gmail.com>
2020-06-19 09:54:45 -0400

    Removing Hello World

--- environments/centos_keycloak/build/1_installpackages.sh
+++
@@ -1,1 +0,0 @@
-dnf install -y podman
--- environments/centos_keycloak/build/2_pulldockerimages.sh
+++
@@ -1,2 +0,0 @@
-podman pull quay.io/keycloak/keycloak
-podman run -d -p 8081:8080 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin -e DB_VENDOR=H2  quay.io/keycloak/keycloak
