1d5a58550d7846fe7c7625d6d24866db7ef920f3
James Land <hokie10@gmail.com>
2020-06-19 11:01:56 -0400

    OpenAPI Security/

--- OpenAPI Security/background.sh
+++ OpenAPI Security/background.sh
@@ -2,6 +2,10 @@ JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
 export JAVA_HOME 
 
 
+git clone https://github.com/jland-redhat/rhc_openapi_todo.git
+cd rhc_openapi_todo
+git checkout security
+
 
 docker run -d -p 8081:8080 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin -e DB_VENDOR=H2  jboss/keycloak
 
