297d96ffc97b6697a4fd83e5fbb7e5ef6c007bb4
James Land <hokie10@gmail.com>
2020-06-19 10:50:45 -0400

    Testing

--- OpenAPI Security/background.sh
+++ OpenAPI Security/background.sh
@@ -3,8 +3,7 @@ export JAVA_HOME
 
 
 
-docker pull quay.io/keycloak/keycloak
-docker run -d -p 8081:8080 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin -e DB_VENDOR=H2  quay.io/keycloak/keycloak
+docker run -d -p 8081:8080 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin -e DB_VENDOR=H2  jboss/keycloak
 
 
 clear
