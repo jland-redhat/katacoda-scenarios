e31f5a40de276edd17ec2046190d62975e399891
James Land <hokie10@gmail.com>
2020-06-22 13:40:19 -0400

    Updating

--- OpenAPI Security/background.sh
+++ OpenAPI Security/background.sh
@@ -7,7 +7,13 @@ cd rhc_openapi_todo
 git checkout security
 
 
-docker run -d -p 8443:8443 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin -e DB_VENDOR=H2  jboss/keycloak
+#docker run -d -p 8443:8443 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin -e DB_VENDOR=H2  jboss/keycloak
+cd /tmp
+curl https://downloads.jboss.org/keycloak/7.0.0/keycloak-7.0.0.zip --output keycloak-7.0.0.zip
+unzip keycloak-7.0.0.zip
+cd keycloak-7.0.0/bin
+./add-user-keycloak.sh -r master -u admin -p admin
 
+cd ~/rhc_openapi_todo
 
 clear
